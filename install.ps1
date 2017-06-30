7c726d88cf3af4378bd2e3c199f7e23057c21f7a
Pierce Boggan <pierceboggan@users.noreply.github.com>
2017-06-30 16:09:26 -0500

    Add PowerShell script for reset, installation, and packaging on Windows.

---
+++ install.ps1
@@ -0,0 +1,81 @@
+param
+(
+  [bool]$install = $False,
+  [bool]$package = $False,
+  [bool]$reset = $False
+)
+
+function reset_package()
+{
+    Write-Host "===Reset Intalled Templates==="
+    Write-Host ""
+
+    Invoke-Expression "dotnet new --debug:reinit"
+
+    Write-Host "Removed user-defined templates."
+    Write-Host ""
+}
+
+function make_package()
+{
+    Write-Host "===Packaging Templates==="
+    Write-Host ""
+
+    Invoke-Expression "nuget pack xamarin-templates.nuspec"
+
+    Write-Host "Successfully packaged templates."
+    Write-Host ""
+}
+
+function install_package()
+{
+    $install_path = Get-ChildItem -path $Directory -Recurse -Include *.nupkg
+    Write-Host "===Installing Templates==="
+    Write-Host ""
+
+    Write-Host ""
+    Invoke-Expression "dotnet new --install $install_path"
+    Write-Host ""
+    Write-Host "Successfully installed templates."
+    Write-Host ""
+}
+
+If ($install -eq $False -and $reset -eq $False -and $package -eq $False) {
+    Set-Variable -Name "install" -Value $True
+    Set-Variable -Name "reset" -Value $True
+    Set-Variable -Name "package" -Value $True
+}
+
+Write-Host ""
+Write-Host "===Prerequisites==="
+
+$TARGETDIR = 'C:\Program Files\dotnet\sdk'
+if (!(Test-Path -Path $TARGETDIR))
+{
+    Write-Host "Hold up..."
+    Write-Host "dotnet isn't installed. You must install .NET Core to use the dotnet new command."
+    Write-Host "Go do that and try again."
+    exit
+}
+
+Write-Host "Found the dotnet CLI at C:\Program Files\dotnet\sdk."
+Write-Host ""
+
+# --reset
+
+if ($reset -eq $True)
+{
+    reset_package
+}
+
+# --package
+if ($package -eq $True)
+{
+    make_package
+}
+
+# --install
+if ($install -eq $True)
+{
+    install_package
+}
