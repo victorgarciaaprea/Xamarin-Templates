786b647bda74e8803617ba61750d2a16c8dc271d
Jimmy Garrido <jimmygarrido@outlook.com>
2018-01-22 10:43:35 -0800

    Convert backend project to ASP.NET Core (#116)

--- multiplatform/content/forms/master-detail/BlankApp.MobileAppService/Controllers/ItemController.cs
+++ multiplatform/content/forms/master-detail/BlankApp.MobileAppService/Controllers/ItemController.cs
@@ -1,56 +1,76 @@
-﻿using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Controllers;
-using System.Web.Http.OData;
-using Microsoft.Azure.Mobile.Server;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
-using BlankApp.MobileAppService.DataObjects;
-using BlankApp.MobileAppService.Models;
+using BlankApp.Models;
 
-namespace BlankApp.MobileAppService.Controllers
+namespace BlankApp.Controllers
 {
-    // TODO: Uncomment [Authorize] attribute to require user be authenticated to access Item(s).
-    // [Authorize]
-    public class ItemController : TableController<Item>
-    {
-        protected override void Initialize(HttpControllerContext controllerContext)
-        {
-            base.Initialize(controllerContext);
-            MasterDetailContext context = new MasterDetailContext();
-            DomainManager = new EntityDomainManager<Item>(context, Request);
-        }
+    [Route("api/[controller]")]
+    public class ItemController : Controller
+	{
 
-        // GET tables/Item
-        public IQueryable<Item> GetAllItems()
-        {
-            return Query();
-        }
+		private readonly IItemRepository ItemRepository;
 
-        // GET tables/Item/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public SingleResult<Item> GetItem(string id)
-        {
-            return Lookup(id);
-        }
+		public ItemController(IItemRepository itemRepository)
+		{
+			ItemRepository = itemRepository;
+		}
 
-        // PATCH tables/Item/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task<Item> PatchItem(string id, Delta<Item> patch)
-        {
-            return UpdateAsync(id, patch);
-        }
+		[HttpGet]
+		public IActionResult List()
+		{
+			return Ok(ItemRepository.GetAll());
+		}
 
-        // POST tables/Item
-        public async Task<IHttpActionResult> PostItem(Item item)
-        {
-            Item current = await InsertAsync(item);
-            return CreatedAtRoute("Tables", new { id = current.Id }, current);
-        }
+		[HttpGet("{Id}")]
+		public Item GetItem(string id)
+		{
+			Item item = ItemRepository.Get(id);
+			return item;
+		}
 
-        // DELETE tables/Item/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task DeleteItem(string id)
-        {
-            return DeleteAsync(id);
-        }
-    }
+		[HttpPost]
+		public IActionResult Create([FromBody]Item item)
+		{
+			try
+			{
+				if (item == null || !ModelState.IsValid)
+				{
+					return BadRequest("Invalid State");
+				}
+
+				ItemRepository.Add(item);
+
+			}
+			catch (Exception)
+			{
+				return BadRequest("Error while creating");
+			}
+			return Ok(item);
+		}
+
+		[HttpPut]
+		public IActionResult Edit([FromBody] Item item)
+		{
+			try
+			{
+				if (item == null || !ModelState.IsValid)
+				{
+					return BadRequest("Invalid State");
+				}
+				ItemRepository.Update(item);
+			}
+			catch (Exception)
+			{
+				return BadRequest("Error while creating");
+			}
+			return NoContent();
+		}
+
+		[HttpDelete("{Id}")]
+		public void Delete(string id)
+		{
+			ItemRepository.Remove(id);
+		}
+	}
 }
