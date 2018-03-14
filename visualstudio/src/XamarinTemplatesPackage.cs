4235ddb1ea95dd3ac24ab1492a8e87ed01d3d4d0
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2018-03-14 15:51:21 -0300

    [loc] - Added localization for all Project and Item templates
    - Added .resx entries
    - Added XamarinTemplatesPackage to have a Package Guid to link from the .vstemplate files
    - Linked .resx entries and Package Guid from all .vstemplate files

---
+++ visualstudio/src/XamarinTemplatesPackage.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Xamarin.Templates
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Guid(PackageGuidString)]
+    public class XamarinTemplatesPackage : Package
+    {
+        public const string PackageGuidString = "4661F042-BFB2-47D0-A4AD-7E1D3002B8F1";
+
+        protected override void Initialize() => base.Initialize();
+    }
+}
