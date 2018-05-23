f351002876aa51f5692ad4dbcab2ff4cc83ef40d
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2018-05-23 12:36:56 -0300

    Fixed watchOS template class namespaces to match template.json SourceName property
    - This lets the template engine to correctly replace the source namespaces with the current App name
    - This fixes Bug #620303 - Namespace mismatch after adding watchOS project to iOS app: https://devdiv.visualstudio.com/DevDiv/XamarinVS/_workitems/edit/620303

--- watchos/content/blank/NewApp.WatchOSApp/Properties/AssemblyInfo.cs
+++ watchos/content/blank/NewApp.WatchOSApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blank.WatchOSApp")]
+[assembly: AssemblyTitle("NewApp.WatchOSApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blank.WatchOSApp")]
+[assembly: AssemblyProduct("NewApp.WatchOSApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
