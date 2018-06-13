466b4ed952f2e25b19cc4400d4f79ffe20465ff8
joj <joj@microsoft.com>
2018-06-13 11:22:04 -0300

    [VS] Made package load async

--- visualstudio/src/XamarinTemplatesPackage.cs
+++ visualstudio/src/XamarinTemplatesPackage.cs
@@ -4,17 +4,15 @@ using System.Runtime.InteropServices;
 
 namespace Xamarin.Templates
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("Xamarin Templates", "Templates for building iOS, Android, and Windows apps with Xamarin and Xamarin.Forms.",
         ThisAssembly.Git.SemVer.Major + "." +
         ThisAssembly.Git.SemVer.Minor + "." +
         ThisAssembly.Git.SemVer.Patch + ThisAssembly.Git.SemVer.DashLabel + " (" +
         ThisAssembly.Git.Commit + ")")]
     [Guid(PackageGuidString)]
-    public class XamarinTemplatesPackage : Package
+    public class XamarinTemplatesPackage : AsyncPackage
     {
         public const string PackageGuidString = "4661F042-BFB2-47D0-A4AD-7E1D3002B8F1";
-
-        protected override void Initialize() => base.Initialize();
     }
 }
