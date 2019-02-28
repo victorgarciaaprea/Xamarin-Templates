81a1ca6f30b45331416f0061df7dbcb3d6385efa
Alan McGovern <alan@xamarin.com>
2019-02-28 21:51:54 +0000

    [build] Add DesignTimeVisible(true) to some templates
    Also include the verbiage to direct people to the docs for
    more information.

--- multiplatform/content/forms/master-detail/NewApp/Views/MainPage.xaml.cs
+++ multiplatform/content/forms/master-detail/NewApp/Views/MainPage.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using NewApp.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace NewApp.Views
 {
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible (true)]
     public partial class MainPage : MasterDetailPage
     {
         Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
--- multiplatform/content/forms/master-detail/NewApp/Views/NewItemPage.xaml.cs
+++ multiplatform/content/forms/master-detail/NewApp/Views/NewItemPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,6 +9,9 @@ using NewApp.Models;
 
 namespace NewApp.Views
 {
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible (true)]
     public partial class NewItemPage : ContentPage
     {
         public Item Item { get; set; }
